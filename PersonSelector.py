ecfc6f99552b5479cfae34b5d0b800d73233c2da
Diego Marvid <diegomarvid99@hotmail.com>
2021-10-05 16:54:13 -0300

    Mejore PersonSelector con funciones

--- PersonSelector.py
+++ PersonSelector.py
@@ -9,27 +9,80 @@ class PersonSelector:
         self.last_index = 0
         self.start_time = 0
         self.current_time = 0
+        self.hit_persons = []
         
+    def MaximumTimeHasPassed(self):  
+        return self.current_time - self.start_time >= self.time_selected
 
+    def ChangeIndex(self, msg):
+
+        self.SetLastIndex(msg)
+
+        if self.current_index < self.last_index:
+            self.current_index += 1
+            self.start_time = self.current_time
+
+        elif self.current_index == self.last_index:
+            self.current_index = 0
+            self.start_time = self.current_time
+
+    def SetLastIndex(self, msg):
+        self.last_index = len(msg) - 1
+
+
+    def GetPersonInIndex(self, msg):
+        if len(msg) > 0:
+            return msg[self.current_index]
+
+    def PersonHasNotBeenHitted(self, person):
+        return person not in self.hit_persons
+
+    def GotAHit(self, msg):
+        HittedPerson = self.GetPersonInIndex(msg)
+        self.hit_persons.append(HittedPerson)
+
+    def ChangeIndexToFirstAvailable(self, msg):
+
+        foundPerson = False
+
+        begin_index = self.current_index
+
+        while not foundPerson:
+
+            self.ChangeIndex(msg)
+
+            nextPerson = self.GetPersonInIndex(msg)
+
+            foundPerson = self.PersonHasNotBeenHitted(nextPerson)
+
+            #Si ya recorri la lista y volvi al mismo punto significa
+            #Que todos estan manchados
+            if begin_index == self.current_index:
+                foundPerson = True
+                #Hacer algo
+
+            
+
+         
     def Select(self, msg):
 
         self.current_time = time.monotonic()
 
-        if self.current_time - self.start_time >= self.time_selected:
+        if self.MaximumTimeHasPassed():
 
-            self.last_index = len(msg) - 1
+            self.ChangeIndex(msg)
 
-            if self.current_index < self.last_index:
-                self.current_index = self.current_index + 1
-                self.start_time = self.current_time
+        # Aca esta trackeando a la persona en self.current_index
+        PersonaTrackeada = self.GetPersonInIndex(msg)
 
-            elif self.current_index == self.last_index:
-                self.current_index = 0
-                self.start_time = self.current_time
-
-        if len(msg) > 0:
-            return msg[self.current_index]
-
+        if self.PersonHasNotBeenHitted(PersonaTrackeada):
+            return PersonaTrackeada
+        else:
+            #Cambio el indice al primero que no este manchado
+            self.ChangeIndexToFirstAvailable(msg)
+            #Ahora trackeo a esta persona
+            PersonaTrackeada = self.GetPersonInIndex(msg)
+            return PersonaTrackeada
 
             
 
--- test_person_order.py
+++ test_person_order.py
@@ -6,7 +6,7 @@ p1 = Person([98,0,3000,0])
 p2 = Person([-99,0,3000,1])
 p3 = Person([200,20,550,2])
 
-persons = [p2, p1, p3]
+persons = [p2, p3]
 
 persons_sorted = sorted(persons)
 persons_sorted = persons_sorted[0:2]
@@ -14,3 +14,5 @@ persons_sorted = persons_sorted[0:2]
 for person in persons_sorted:
     
     print(person)
+
+print(p1 not in persons_sorted)
